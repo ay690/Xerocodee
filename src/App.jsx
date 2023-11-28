7fbd96e96fb53778fd66e4ea521f814bc9296f2c
Aniket Yadav <aniketyadav690@gmail.com>
2023-11-29 01:23:47 +0530

    starter of xerocodee

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-2xl font-bold underline">hello!</div>
+    </>
+  );
+}
+
+export default App;
